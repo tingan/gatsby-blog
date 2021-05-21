9a98fcab7060c0a01aa9b056074a25ee8dfe7d5f
tingan <tingan@wikiwebguides.com>
2021-05-21 11:13:01 +0300

    Finished Gatsby Tutorial Part2

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>About me</p>
+    </Layout>
+  );
+};
+export default AboutPage;
